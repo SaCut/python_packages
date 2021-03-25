744a90084f867159c5abdc1aef4a9c27c74a36fe
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-25 15:19:49 +0000

    updated README.md, python_api.py

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Python packages
 
 ### What is a package
+- A package is a set of useful functions and classes that are useful for common tasks
 
 ### Why should we use them?
+- Because they make writing code orders of magnitude easier and faster.
+Instead of having to write the same code again and again, or putting energy and time into creating a codebase ex-novo, it's much more convenient to import packages that have already been tested and that have been improved to the point of reaching the best computational efficiency and speed possible.
 
 ### What is pip?
 - `pip` is a package managed in Python, used to install any packages
--- python_api.py
+++ python_api.py
@@ -4,23 +4,35 @@
 # we will connect to www.bbc.co.uk and check if the website is working
 import requests
 
-# responses = requests.get("http://www.bbc.co.uk/")
+response = requests.get("http://www.bbc.co.uk/")
 
 # if responses.status_code == 200:
 # 	print("the website is up and running: status code " + str(responses.status_code))
 # else:
 # 	print("Oops, something went wrong! status code: " + str(responses.status_code))
 
-#response code 200 means everything is working correctly
+# #response code 200 means everything is working correctly
 
-# test: create a function called status_code_check
-# function should return status code with appropriate message
-def status_code_check(site):
-	responses = requests.get(site)
+# # test: create a function called status_code_check
+# # function should return status code with appropriate message
+# def status_code_check(site):
+# 	responses = requests.get(site)
 
-	if responses.status_code == 200:
-		return "the website is up and running: status code " + str(responses.status_code)
-	else:
-		return "Oops, something went wrong! status code: " + str(responses.status_code)
+# 	if responses.status_code == 200:
+# 		return "the website is up and running: status code " + str(responses.status_code)
+# 	else:
+# 		return "Oops, something went wrong! status code: " + str(responses.status_code)
 
-print(status_code_check("http://www.bbc.co.uk/"))
+# print(status_code_check("http://www.bbc.co.uk/"))
+
+if response:
+	print("success, and the status code is: " + response.status_code)
+else:
+	print("failure. The status code is: " + response.status_code)
+
+
+# requests goes one step further in simplifying this process for us
+# if you use the response package in a condition expression
+# it will evaluate to True if the status code was between 200 and 400, False otherwise
+
+# 
