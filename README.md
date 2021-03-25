89dc535e9c70fbb2738927fecac3f3f85d039507
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-25 15:57:34 +0000

    modified README.md and python_api.py

--- README.md
+++ README.md
@@ -7,6 +7,19 @@
 - Because they make writing code orders of magnitude easier and faster.
 Instead of having to write the same code again and again, or putting energy and time into creating a codebase ex-novo, it's much more convenient to import packages that have already been tested and that have been improved to the point of reaching the best computational efficiency and speed possible.
 
+##### Requests
+- The package `requests` gives a splendid example of why we should use packages. One of the features that the creators of the package have implemented under the hood, is that it's possible to pass a `requests` object directly as a conditional:
+```python
+import requests
+
+content = requests.get("a_URL")
+
+if content:
+	some_code
+```
+If the result of the `get()` function was a status code between 200 and 400 (normal functioning page), the object will be evaluated as `True`, and it will be evaluated as `False` otherwise.
+This implementation is a significant help in coding terms, but it would never have been realistic to implement it if the code was written directly. 
+
 ### What is pip?
 - `pip` is a package managed in Python, used to install any packages
 - we use `pip install package_name` to install a package in Python2, `pip3 install package_name` in Python3
--- python_api.py
+++ python_api.py
@@ -6,24 +6,24 @@ import requests
 
 response = requests.get("http://www.bbc.co.uk/")
 
-# if responses.status_code == 200:
-# 	print("the website is up and running: status code " + str(responses.status_code))
-# else:
-# 	print("Oops, something went wrong! status code: " + str(responses.status_code))
+if responses.status_code == 200:
+	print("the website is up and running: status code " + str(responses.status_code))
+else:
+	print("Oops, something went wrong! status code: " + str(responses.status_code))
 
-# #response code 200 means everything is working correctly
+#response code 200 means everything is working correctly
 
-# # test: create a function called status_code_check
-# # function should return status code with appropriate message
-# def status_code_check(site):
-# 	responses = requests.get(site)
+# test: create a function called status_code_check
+# function should return status code with appropriate message
+def status_code_check(site):
+	responses = requests.get(site)
 
-# 	if responses.status_code == 200:
-# 		return "the website is up and running: status code " + str(responses.status_code)
-# 	else:
-# 		return "Oops, something went wrong! status code: " + str(responses.status_code)
+	if responses.status_code == 200:
+		return "the website is up and running: status code " + str(responses.status_code)
+	else:
+		return "Oops, something went wrong! status code: " + str(responses.status_code)
 
-# print(status_code_check("http://www.bbc.co.uk/"))
+print(status_code_check("http://www.bbc.co.uk/"))
 
 if response:
 	print("success, and the status code is: " + response.status_code)
@@ -34,5 +34,3 @@ else:
 # requests goes one step further in simplifying this process for us
 # if you use the response package in a condition expression
 # it will evaluate to True if the status code was between 200 and 400, False otherwise
-
-# 
