2bbc7cc7860127c72e7a23a3ecefa24f2fd53e69
Saverio <saveriocutrupi@users.noreply.github.com>
2021-03-25 14:29:47 +0000

    added python_api.py, python_packages.py

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python packages
+
+### What is a package
+
+### Why should we use them?
+
+### What is pip?
+- `pip` is a package managed in Python, used to install any packages
+- we use `pip install package_name` to install a package in Python2, `pip3 install package_name` in Python3
---
+++ python_packages.py
@@ -0,0 +1,32 @@
+from random import random # generates random numbers
+import math
+
+# avg = 0
+
+# for _ in range(1000):
+# 	avg += (random()*100)
+
+# print(round(avg/1000))
+
+# num_float = 23.6
+
+# print("Actual float value" + str(num_float))
+
+# print(math.ceil(num_float)) # rounds the float up
+# print(math.floor(num_float)) # rounds the float down
+# print(round(num_float)) # rounds up if over 5, down otherwise
+
+# Python modules
+import os, sys, datetime
+
+# working_dir = os.getcwd() # gives back the curernt working directory
+# print(working_dir)
+
+# print(os.uname())
+
+# print(os.cpu_count())
+
+# print(datetime.datetime.today())
+
+print(sys.path)
+
